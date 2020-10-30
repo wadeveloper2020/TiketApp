79294a78ad55330f04ccdcacfa44f680a148f5ab
wadeveloper2020 <wa.dev2020@gmail.com>
2020-10-30 20:20:34 +0700

    Update java dan xml pada EditProfileAct

--- app/src/main/java/com/example/tikets/EditProfileAct.java
+++ app/src/main/java/com/example/tikets/EditProfileAct.java
@@ -1,30 +1,47 @@
 package com.example.tikets;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.github.florent37.shapeofview.shapes.CircleView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class EditProfileAct extends AppCompatActivity {
 
     EditText xnama_lengkap, xbio, xusername, xpassword, xemail_address;
-
-    Button btn_save;
+    ImageView photo_edit_profile;
+    Button btn_save, btn_add_photo_new;
     LinearLayout btn_back;
 
     DatabaseReference reference;
+    StorageReference storage;
+
+    Uri photo_location;
+    Integer photo_max = 1;
 
     String USERNAME_KEY = "usernamekey";
     String username_key = "";
@@ -42,8 +59,10 @@ public class EditProfileAct extends AppCompatActivity {
         xusername = findViewById(R.id.xusername);
         xpassword = findViewById(R.id.xpassword);
         xemail_address = findViewById(R.id.xemail_address);
+        photo_edit_profile = findViewById(R.id.photo_edit_profile);
 
         btn_save = findViewById(R.id.btn_save);
+        btn_add_photo_new = findViewById(R.id.btn_add_photo_new);
         btn_back = findViewById(R.id.back_myprofile);
 
         reference = FirebaseDatabase.getInstance().getReference().child("Users").child(username_key_new);
@@ -55,6 +74,8 @@ public class EditProfileAct extends AppCompatActivity {
                 xusername.setText(snapshot.child("username").getValue().toString());
                 xpassword.setText(snapshot.child("password").getValue().toString());
                 xemail_address.setText(snapshot.child("email_address").getValue().toString());
+                Picasso.get().load(snapshot.child("url_photo_profile")
+                        .getValue().toString()).centerCrop().fit().into(photo_edit_profile);
             }
 
             @Override
@@ -66,9 +87,14 @@ public class EditProfileAct extends AppCompatActivity {
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                btn_save.setEnabled(true);
+                btn_save.setText("Loading...");
+
+                reference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
+
                         reference.getRef().child("nama_lengkap").setValue(xnama_lengkap.getText().toString());
                         reference.getRef().child("bio").setValue(xbio.getText().toString());
                         reference.getRef().child("username").setValue(xusername.getText().toString());
@@ -81,15 +107,67 @@ public class EditProfileAct extends AppCompatActivity {
 
                     }
                 });
+                storage = FirebaseStorage.getInstance().getReference()
+                        .child("PhotoUsers").child(username_key_new);
 
-                Intent intent = new Intent(EditProfileAct.this, MyProfileAct.class);
-                startActivity(intent);
+                if (photo_location != null){
+                    StorageReference storageReference = storage.child(System.currentTimeMillis()
+                            +"."+ getFileExtention(photo_location));
+
+                    storageReference.putFile(photo_location)
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Task<Uri> uriTask=taskSnapshot.getMetadata().getReference().getDownloadUrl();                                               while(!uriTask.isSuccessful());
+                                    Uri downloadUri=uriTask.getResult();
+                                    final String download_url=downloadUri.toString();
+
+                                    //String url_photo = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                                    reference.getRef().child("url_photo_profile").setValue(download_url);
+
+                                }
+                            }).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            Intent intent = new Intent(EditProfileAct.this, MyProfileAct.class);
+                            startActivity(intent);
+                        }
+                    });
+                }
             }
         });
 
-
-
+        btn_add_photo_new.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPhoto();
+            }
+        });
     }
+
+    String getFileExtention(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    public void findPhoto(){
+        Intent pic = new Intent();
+        pic.setType("image/*");
+        pic.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(pic,photo_max );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==photo_max && resultCode == RESULT_OK && data!= null && data.getData() != null)
+        {
+            photo_location = data.getData();
+            Picasso.get().load(photo_location).centerCrop().fit().into(photo_edit_profile);
+        }
+    }
+
     public void getUserNameLokal(){
         SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
         username_key_new = sharedPreferences.getString(username_key,"");
--- app/src/main/java/com/example/tikets/RegisterOneAct.java
+++ app/src/main/java/com/example/tikets/RegisterOneAct.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,7 +27,7 @@ public class RegisterOneAct extends AppCompatActivity {
     String USERNAME_KEY = "usernamekey";
     String username_key ="";
 
-    DatabaseReference reference;
+    DatabaseReference reference,reference_username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +41,56 @@ public class RegisterOneAct extends AppCompatActivity {
         password = findViewById(R.id.password);
         email_address = findViewById(R.id.email_address);
 
+
+
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(username_key, username.getText().toString());
-                editor.apply();
 
-                reference = FirebaseDatabase.getInstance().getReference()
-                        .child("Users").child(username.getText().toString());
-                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                btn_continue.setEnabled(false);
+                btn_continue.setText("Loading...");
+
+                reference_username = FirebaseDatabase.getInstance()
+                        .getReference().child("Users")
+                        .child(username.getText().toString());
+
+                reference_username.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        snapshot.getRef().child("username").setValue(username.getText().toString());
-                        snapshot.getRef().child("password").setValue(password.getText().toString());
-                        snapshot.getRef().child("email_address").setValue(email_address.getText().toString());
-                        snapshot.getRef().child("user_balance").setValue(800);
+                        if (snapshot.exists()){
+
+                            Toast.makeText(RegisterOneAct.this, "Username sudah terdaftar!", Toast.LENGTH_SHORT).show();
+
+                            btn_continue.setEnabled(true);
+                            btn_continue.setText("CONTINUE");
+
+                        }else {
+                            SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString(username_key, username.getText().toString());
+                            editor.apply();
+
+                            reference = FirebaseDatabase.getInstance().getReference()
+                                    .child("Users").child(username.getText().toString());
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    snapshot.getRef().child("username").setValue(username.getText().toString());
+                                    snapshot.getRef().child("password").setValue(password.getText().toString());
+                                    snapshot.getRef().child("email_address").setValue(email_address.getText().toString());
+                                    snapshot.getRef().child("user_balance").setValue(800);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+                            Intent intent = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
+                            startActivity(intent);
+
+                        }
                     }
 
                     @Override
@@ -64,9 +98,6 @@ public class RegisterOneAct extends AppCompatActivity {
 
                     }
                 });
-
-                Intent intent = new Intent(RegisterOneAct.this, RegisterTwoAct.class);
-                startActivity(intent);
             }
         });
 
